7e005db6f4e569a803a96b518f96e4d898dec492
ventaquil <konrad@golawski.it>
2018-11-20 20:46:44 +0100

    GH-2 Add speech support to MainPage

--- Cinema/Cinema/MainPage.xaml.cs
+++ Cinema/Cinema/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Speech.Recognition;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,26 +21,81 @@ namespace Cinema
     /// <summary>
     /// Interaction logic for MainPage.xaml
     /// </summary>
-    public partial class MainPage : Page
+    public partial class MainPage : Page, SpeechControllable
     {
-        private Window window;
         private SqlConnection dbConnection;
+        private SpeechRecognitionEngine speechRecognitionEngine;
+        private Window window;
 
         public MainPage(Window window, SqlConnection dbConnection)
         {
             this.window = window;
             this.dbConnection = dbConnection;
+
             InitializeComponent();
+
+            InitializeSpeechRecognition();
+
+            EnableSpeechRecognition();
+        }
+
+        private void ChangePage(Page page)
+        {
+            StopSpeechRecognition();
+
+            window.Content = page;
+        }
+
+        public void EnableSpeechRecognition()
+        {
+            speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        public Grammar GetSpeechGrammar()
+        {
+            Choices choices = new Choices();
+            choices.Add("Raz");
+            choices.Add("Dwa");
+            choices.Add("Trzy");
+
+            GrammarBuilder grammarBuilder = new GrammarBuilder(choices);
+
+            return new Grammar(grammarBuilder);
+        }
+
+        public void InitializeSpeechRecognition()
+        {
+            CultureInfo cultureInfo = new CultureInfo("pl-PL");
+
+            speechRecognitionEngine = new SpeechRecognitionEngine(cultureInfo);
+            speechRecognitionEngine.LoadGrammarAsync(GetSpeechGrammar());
+            speechRecognitionEngine.SetInputToDefaultAudioDevice();
+            speechRecognitionEngine.SpeechRecognized += SpeechRecognitionEngine_SpeechRecognized;
         }
 
         private void OrderButton_Click(object sender, RoutedEventArgs e)
         {
-            window.Content = new OrderPage(window, this, dbConnection);
+            ChangePage(new OrderPage(window, this, dbConnection));
         }
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-            window.Content = new SearchPage(window, this, dbConnection);
+            ChangePage(new SearchPage(window, this, dbConnection));
+        }
+
+        private void SpeechRecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine(e.Result.Text);
+        }
+
+        public void StopSpeechRecognition()
+        {
+            speechRecognitionEngine.RecognizeAsyncCancel();
+        }
+
+        public void WaitForSpeechRecognition()
+        {
+            speechRecognitionEngine.RecognizeAsyncStop();
         }
     }
 }
