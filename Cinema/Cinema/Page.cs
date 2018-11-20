7f96dc524082b16dcb4e5ed8ef69f213962c8255
ventaquil <konrad@golawski.it>
2018-11-20 21:05:01 +0100

    GH-2 Add custom Page class

---
+++ Cinema/Cinema/Page.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cinema
+{
+    public partial class Page : System.Windows.Controls.Page
+    {
+        private readonly Page previousPage;
+
+        private readonly SqlConnection sqlConnection;
+
+        private readonly Window window;
+
+        public Page(Window window, SqlConnection sqlConnection) : this(window, null, sqlConnection)
+        {
+        }
+
+        public Page(Window window, Page previousPage, SqlConnection sqlConnection)
+        {
+            this.window = window;
+
+            this.previousPage = previousPage;
+
+            this.sqlConnection = sqlConnection;
+        }
+
+        protected void ChangePage(Page page)
+        {
+            if (page == null)
+            {
+                throw new NullReferenceException("Page cannot be null.");
+            }
+
+            if (this is SpeechControllable)
+            {
+                ((SpeechControllable)this).StopSpeechRecognition();
+            }
+
+            window.Content = page;
+
+            if (page is SpeechControllable)
+            {
+                ((SpeechControllable)page).EnableSpeechRecognition();
+            }
+        }
+
+        protected void MoveBack()
+        {
+            ChangePage(previousPage);
+        }
+    }
+}
