e28dda8f44ce3940f818fc1b058fcd4d4fc2c490
ventaquil <konrad@golawski.it>
2018-11-24 20:14:57 +0100

    GH-10 Change DescriptionPage into SpeechPage

--- Cinema/Cinema/DescriptionPage.xaml.cs
+++ Cinema/Cinema/DescriptionPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Speech.Recognition;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace Cinema
     /// <summary>
     /// Logika interakcji dla klasy DescriptionPage.xaml
     /// </summary>
-    public partial class DescriptionPage : Page
+    public partial class DescriptionPage : SpeechPage
     {
         private readonly Movie Movie;
 
@@ -46,6 +47,53 @@ namespace Cinema
         private void OrderButton_Click(object sender, RoutedEventArgs e)
         {
             ChangePage(new MovieHoursPage(window, previousPage, sqlConnectionFactory, Movie));
+        }
+        
+        private void SpeakHelp()
+        {
+            Speak("Pomoc");
+        }
+
+        private void SpeakRepeat()
+        {
+            Speak("Powtórz proszę.");
+        }
+
+        private void SpeakQuit()
+        {
+            Speak("Zapraszam ponownie.");
+        }
+
+        protected override void SpeechRecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            base.SpeechRecognitionEngine_SpeechRecognized(sender, e);
+
+            RecognitionResult result = e.Result;
+
+            if (result.Confidence < 0.6)
+            {
+                SpeakRepeat();
+            }
+            else
+            {
+                string[] command = result.Semantics.Value.ToString().ToLower().Split('.');
+                DispatchAsync(() =>
+                {
+                    switch (command.First())
+                    {
+                        case "back":
+                            MoveBack();
+                            break;
+                        case "help":
+                            SpeakHelp();
+                            break;
+                        case "quit":
+                            SpeakQuit();
+                            Close();
+                            break;
+                    }
+                });
+            }
         }
     }
 }
