546766aa63797e6a61adbeb4b84d1ea9eebfa5bf
ventaquil <konrad@golawski.it>
2018-11-28 23:02:37 +0100

    Update project structure

--- Cinema/Cinema/Properties/Queries/DropTables.sql
+++
@@ -1,8 +0,0 @@
-﻿drop table Tickets;
-drop table Screenings;
-drop table Seats;
-drop table Auditoriums;
-drop table Prices;
-drop table Tags;
-drop table Genres;
-drop table Movies;
--- Cinema/QueriesSQL/Procedures/procedure_GetMoviesByGenre.sql
+++
@@ -1,8 +0,0 @@
-﻿create   procedure procedure_GetMoviesByGenre 
-	@genre varchar(100)
-as
-	select distinct movies.title 
-		from Tags, Genres, 
-		(select Movies.id, Movies.title as title from Movies, Screenings where Movies.id = Screenings.movieID and Screenings.screeningDate = CONVERT(date,  GETDATE())) movies
-		where	Genres.id = Tags.genreID and 
-				Tags.movieID = movies.id and Genres.genre = @genre;
